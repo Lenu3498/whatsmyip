37aaf5b5ff1c146d80d89c5fba137e420c73e673
Lena <l.ernst3498@gmail.com>
2021-05-10 23:16:20 +0200

    Fix position on map.js and format code with prettier

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import Ip from "./components/Ip/Ip.js";
-import Map from "./components/Map/Map.js";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Ip from './components/Ip/Ip.js';
+import Map from './components/Map/Map.js';
 import './App.css';
-
 
 const API_KEY = process.env.REACT_APP_IPFY_API_KEY;
 const API_URL = `https://geo.ipify.org/api/v1?apiKey=${API_KEY}`;
@@ -15,48 +14,46 @@ const App = () => {
   const [lat, setLat] = useState([]);
   const [lng, setLng] = useState([]);
 
-useEffect(() => {
-  async function fetchIp() {
-    setIsLoading(true);
-    try {
-      axios.get(API_URL).then((result) => {
-        console.log(result.data)
-        const userIp = result.data.ip;
-        const userLng = result.data.location.lng;
-        const userLat = result.data.location.lat;
-        setIpAddress(userIp);
-        setLat(result.data.location.lat);
-        console.log(lat)
-        setLng(result.data.location.lng);
-        console.log(lng);
-        setIsLoading(false);
-        console.log(userLng);
-        console.log(userLat);
-      });
-    } catch (error) {
-      alert("No results");
+  useEffect(() => {
+    async function fetchIp() {
+      setIsLoading(true);
+
+      try {
+        axios.get(API_URL).then((result) => {
+          setIpAddress(result.data.ip);
+          setLat(result.data.location.lat);
+          setLng(result.data.location.lng);
+          setIsLoading(false);
+        });
+      } catch (error) {
+        alert('No results');
+      }
     }
-  }
 
-  fetchIp();
+    fetchIp();
+  }, []);
 
-},[lng, lat, ipAddress]);
+  return (
+    <div className="App">
+      <header>
+        <h1>Your IP is</h1>
+      </header>
 
-
-return (
-  <div className="App">
-
-    <h1>
-      Your IP
-    </h1>
-
-    <Ip ipAddress={ipAddress}  />
-    <Map lat={lat} lng={lng} />
-
-
-
-  </div>
-);
+      <main>
+        {isLoading ? (
+          <div>
+            <h2>Finding your IP address...</h2>
+            <i className="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
+          </div>
+        ) : (
+          <div>
+            <Ip ipAddress={ipAddress} />
+            <Map lat={lat} lng={lng} />
+          </div>
+        )}
+      </main>
+    </div>
+  );
 };
 
 export default App;
